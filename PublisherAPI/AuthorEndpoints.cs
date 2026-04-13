116eb336badc42d783ee844403f733222bd8dd39
Larsa Tahir <larsatahir@gmail.com>
2026-04-14 01:03:37 +0200

    first commit

---
+++ PublisherAPI/AuthorEndpoints.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.OpenApi;
+using PublisherData;
+using PublisherDomain;
+namespace PublisherAPI;
+
+public static class AuthorEndpoints
+{
+    public static void MapAuthorEndpoints (this IEndpointRouteBuilder routes)
+    {
+        var group = routes.MapGroup("/api/Author").WithTags(nameof(Author));
+
+        group.MapGet("/", async (PubContext db) =>
+        {
+            return await db.Authors.Include(a=>a.Books).AsNoTracking().ToListAsync();
+        })
+        .WithName("GetAllAuthors");
+
+        group.MapGet("/{authorid}", async Task<Results<Ok<Author>, NotFound>> (int authorid, PubContext db) =>
+        {
+            return await db.Authors.AsNoTracking()
+                .FirstOrDefaultAsync(model => model.AuthorId == authorid)
+                is Author model
+                    ? TypedResults.Ok(model)
+                    : TypedResults.NotFound();
+        })
+        .WithName("GetAuthorById");
+
+        group.MapPut("/{authorid}", async Task<Results<Ok, NotFound>> (int authorid, Author author, PubContext db) =>
+        {
+            var affected = await db.Authors
+                .Where(model => model.AuthorId == authorid)
+                .ExecuteUpdateAsync(setters => setters
+                    .SetProperty(m => m.AuthorId, author.AuthorId)
+                    .SetProperty(m => m.FirstName, author.FirstName)
+                    .SetProperty(m => m.LastName, author.LastName)
+                    );
+            return affected == 1 ? TypedResults.Ok() : TypedResults.NotFound();
+        })
+        .WithName("UpdateAuthor");
+
+        group.MapPost("/", async (Author author, PubContext db) =>
+        {
+            db.Authors.Add(author);
+            await db.SaveChangesAsync();
+            return TypedResults.Created($"/api/Author/{author.AuthorId}",author);
+        })
+        .WithName("CreateAuthor");
+
+        group.MapDelete("/{authorid}", async Task<Results<Ok, NotFound>> (int authorid, PubContext db) =>
+        {
+            var affected = await db.Authors
+                .Where(model => model.AuthorId == authorid)
+                .ExecuteDeleteAsync();
+            return affected == 1 ? TypedResults.Ok() : TypedResults.NotFound();
+        })
+        .WithName("DeleteAuthor");
+    }
+}
